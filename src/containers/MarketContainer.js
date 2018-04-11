ab2215dd6fc0380d493d5f6c227fc8d792b4f4a1
aksh-github <beatle02@gmail.com>
2018-04-12 01:37:11 +0530

    remove unnecc code

--- src/containers/MarketContainer.js
+++ src/containers/MarketContainer.js
@@ -6,22 +6,23 @@ import _debounce from 'lodash/debounce';
 
 export default class MarketContainer extends React.Component 
 {
-	/*constructor(props, context)
+	constructor(props, context)
 	{
 		super(props, context);
 		this.state = {
 		}
 
-	}*/
+	}
 		
 	componentDidMount()
 	{
-		//this._attachEventHandlers();
+		this._attachEventHandlers();
 		//this._getData();		
 	}
 	
 	componentDidUpdate()
 	{
+		this._attachEventHandlers();
 	}
 	
 	componentWillUnmount()
@@ -33,57 +34,7 @@ export default class MarketContainer extends React.Component
 	{	
 		
 	}//()
-			
-	_getSectorData(forceRefresh)
-	{
-		/*let dt = this.state.sector.lastUpdated, data=this.state.sector.data;
-
-		this.setState({ 
-			sector: { data: null, lastUpdated: '', processing: true }
-		});
-
-		MarketDataStore.getSectorData(forceRefresh)
-		.then( (response)=>
-		{			
-			if(response.data)
-			{				
-				dt = response.lastUpdated;
-
-				this.setState({ 
-					sector: { 
-						data: response.data.crawlResponse, 
-						lastUpdated: dt.getHours() + ':' + dt.getMinutes(),
-						processing: false
-					}
-				});
-
-			}
-				
-			else
-			{
-				this.setState({ 
-					sector: { 
-						data: data, 
-						lastUpdated: dt,
-						processing: false
-					}
-				});
-			}
-				
-			return null;
-		} )
-		.catch( ()=>
-		{
-			this.setState({ 
-				sector: { 
-					data: data, 
-					lastUpdated: dt,
-					processing: false
-				}
-			});
-		} )*/
-
-	}//()
+		
 	
 	_attachEventHandlers()
 	{
@@ -94,60 +45,7 @@ export default class MarketContainer extends React.Component
 		}, 1300, { 'leading': true }) );
 
 	}
-	
-	_btnSectorRefreshClick(e)
-	{		
-		if(this.state.sector.processing)
-			return;
-
-		//foll setstate unnecc.
-
-		/*this.setState({
-			sector: {
-				data: this.state.sector.data, 
-				lastUpdated: this.state.sector.lastUpdated,
-				processing: true
-			}
-		});*/
-
-		this._getSectorData(true);
-
-	}//()
-	
-	_btnWorldRefreshClick(e)
-	{
-		if(this.state.world.processing)
-			return;
-
-		this.setState({
-			world: {
-				data: this.state.world.data, 
-				lastUpdated: this.state.world.lastUpdated,
-				processing: true
-			}
-		});
-
-		this._getWorldData(true);
-		
-	}//()
-	
-	_btnIndiaRefreshClick(e)
-	{		
-		if(this.state.india.processing)
-			return;
-
-		this.setState({
-			india: {
-				data: this.state.india.data, 
-				lastUpdated: this.state.india.lastUpdated,
-				processing: true
-			}
-		});
-
-		this._getIndiaData(true);
-		
-	}//()
-		
+			
 	render() 
 	{
 		return <div className="market">
